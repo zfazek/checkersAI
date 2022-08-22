b55bdcb8226b6ebe210f1379ecd3c694479fe4c6
zfazek <ongroa@gmail.com>
2022-08-22 19:22:57 +0200

    delete c version

--- move.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef struct move_t {
-    int idxs[32];
-    int len;
-} move_t;
--- moves.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "move.h"
-
-void moves_print(int n_moves, move_t *moves);
